2352e5cb779e77c3e5c60331f80a6dcab50a2679
Josh Guffey <joshua.guffey@gmail.com>
2018-08-29 21:49:48 -0700

    starting on video support, not sure if the video filter will subclass AVAsset yet.

---
+++ SnapSliderFilters/Classes/SNVideoFilter.swift
@@ -0,0 +1,13 @@
+//
+//  SNVideoFilter.swift
+//  Pods-SnapSliderFilters_Example
+//
+//  Created by Josh Guffey on 8/29/18.
+//
+
+import UIKit
+import AVKit
+
+open class SNVideoFilter: AVAsset {
+    // Todo.
+}
